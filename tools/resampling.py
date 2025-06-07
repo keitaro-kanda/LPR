e747e000bfbeb074b8e91ae3034cb9d01cacad6d
keitaro kanda MacStudio <{apollo11keitaro@gmail.com}>
2025-06-07 16:14:01 +0900

    出力フォルダパスを修正し、リサンプリング関数の引数を更新。データフィルタリング処理を改善し、チャネル2Bのインデックスを保存する機能を追加。

--- tools/resampling.py
+++ tools/resampling.py
@@ -23,7 +23,7 @@ else:
 
 
 #* Define output folder path
-output_dir = os.path.join(os.path.dirname(data_folder_path), 'Resampled_Data_xml')
+output_dir = os.path.join(os.path.dirname(data_folder_path), 'Resampled_Data')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 txt_output_dir = os.path.join(output_dir, 'txt')
@@ -42,11 +42,9 @@ if not os.path.exists(position_output_dir):
 #data_2B = np.zeros((0, 2048))
 #medf = np.zeros(0)
 
-def resampling(data): # input is 2D array including position data
-    positions = data[:7, :] # data containes header.
-    signals = data[7:, :] # 0-6: velocity and position data
+def resampling(signal_data, position_data, sequence_id): # input is 2D array including position data
     #* Do not consider first 300 datapoints
-    img = signals[300:, :]
+    img = signal_data[300:, :]
 
     #* Compute the derivative of the data in x direction with Sobel with a kernel size of 5
 	#* This should eliminate the horizonatal lines
@@ -57,11 +55,11 @@ def resampling(data): # input is 2D array including position data
     med_denoised = ndimage.median_filter(sobelx, 5)
 
     #* Sum the absolute values of the denoised traces on time dims
-	#* We should have cancelled signals on death traces --> highest value on the real traces
+	#* We should have cancelled signal_data on death traces --> highest value on the real traces
     med = np.sum(np.abs(med_denoised), axis = 0)
 
 	#* Define a moving window to avoid taking into account small portions with high values.
-	#* We want to take the signals only when it has a value highest than thres for window consecutive traces
+	#* We want to take the signal_data only when it has a value highest than thres for window consecutive traces
     window = 25
     thres = 20000
 
@@ -76,63 +74,38 @@ def resampling(data): # input is 2D array including position data
             medf[i : i + window] = 1
 
     idx = np.where(medf == 1)[0]
+
+    #* Save idx when the channel is 2B
+    if channel_name == '2B':
+        idx_dir = os.path.join(output_dir, 'idx')
+        if not os.path.exists(idx_dir):
+            os.makedirs(idx_dir)
+        np.savetxt(idx_dir + '/' + str(sequence_id) + '_idx.txt', idx, delimiter=' ')
+
+    #* Filter the data with the idx
     if np.sum(idx) == 0:
-        data_filtered = signals[:, idx]
-        positions_filtered = positions[:, idx]
+        data_filtered = signal_data[:, idx]
+        position_data_filtered = position_data[:, idx]
         print('No interesting data found')
     else:
-        data_filtered = signals[:, idx]
-        positions_filtered = positions[:, idx]
-        print('Raw data shape was: ', data.shape)
+        data_filtered = signal_data[:, idx]
+        position_data_filtered = position_data[:, idx]
+        print('Raw data shape was: ', signal_data.shape)
         print('Filtered data shape is: ', data_filtered.shape)
 
 
         #* Save filtered data as .txt file
         np.savetxt(txt_output_dir + '/' + str(sequence_id) + '_resampled.txt', data_filtered, delimiter=' ')
-        np.savetxt(position_output_dir + '/' + str(sequence_id) + '_resampled_position.txt', positions_filtered, delimiter=' ')
+        np.savetxt(position_output_dir + '/' + str(sequence_id) + '_resampled_position.txt', position_data_filtered, delimiter=' ')
         header_position = 'velocity, position_x, position_y, position_z, reference_point_x, reference_point_y, reference_point_z'
         np.savetxt(position_output_dir + '/' + str(sequence_id) + '_resampled_position.txt',
-                    positions_filtered, delimiter=' ', header=header_position)
+                    position_data_filtered, delimiter=' ', header=header_position)
 
-    return sobelx, med_denoised, med, medf, data_filtered, positions_filtered
-
-
-#* Condunct resampling
-total_trace_num = 0
-resampled_trace_num = 0
-for ECHO_data in tqdm(natsorted(os.listdir(data_folder_path))):
-    #* Load ECHO data
-    if not ECHO_data.endswith('.txt'):
-        continue
-    if ECHO_data.startswith('._'):
-        continue
-
-    ECHO_data_path = os.path.join(data_folder_path, ECHO_data)
-    raw_data = np.loadtxt(ECHO_data_path, delimiter=' ', skiprows=1) # skip the header
-    sequence_id = ECHO_data.split('_')[-1].split('.')[0]
-
-    print('---------------------------------')
-    print('ECHO data:', ECHO_data)
-    print('record count number:', raw_data.shape[1])
-    #* Count the number of traces before resampling
-    total_trace_num += raw_data.shape[1]
-    print('Now processing...')
-    print('   ')
-
-    sobelx, med_denoised, med, medf, data_filtered, positions_filtered = resampling(raw_data)
-    data_filtered4plot = np.zeros((2048, data_filtered.shape[1]))
-    data_filtered4plot[:, :data_filtered.shape[1]] = data_filtered
+    return sobelx, med_denoised, med, medf, data_filtered, position_data_filtered
 
 
-    #* Count the number of traces after resampling
-    resampled_trace_num += data_filtered.shape[1]
-    if ECHO_data == natsorted(os.listdir(data_folder_path))[-1]:
-        with open(output_dir + '/total_trace_num.txt', 'w') as f:
-            f.write('Number of total traces before resampling: ' + str(total_trace_num) + '\n')
-            f.write('Number of total traces after resampling: '+ str(resampled_trace_num))
-
-
-    #* Plot
+#* Define the plot function for CH-2B
+def plot_2B(raw_data, sobelx, med_denoised, med, medf, data_filtered, sequence_id):
     fig, ax = plt.subplots(6, 1, figsize=(12, 20), tight_layout=True, sharex=True)
     fontsize_large = 20
     fontsize_medium = 18
@@ -165,25 +138,150 @@ for ECHO_data in tqdm(natsorted(os.listdir(data_folder_path))):
     #* Plot median data
     ax[3].plot(med, label='med')
     ax[3].hlines(20000, 0, med.shape[0], 'r', label='thres')
-    ax[3].set_title('Filtered singals amplitude', fontsize=fontsize_medium)
+    ax[3].set_title('Filtered signals amplitude', fontsize=fontsize_large)
     ax[3].legend()
     ax[3].set_ylabel('Amplitude', fontsize=fontsize_medium)
     ax[3].tick_params(axis='both', which='major', labelsize=fontsize_small)
 
     #* Plot median filtered data
     ax[4].plot(medf)
-    ax[4].set_title('Gate with interesting data', fontsize=fontsize_medium)
+    ax[4].set_title('Gate with interesting data', fontsize=fontsize_large)
     ax[4].tick_params(axis='both', which='major', labelsize=fontsize_small)
 
     #* Plot filtered data
-    ax[5].imshow(data_filtered4plot, aspect='auto', cmap='seismic',
-                extent=[0, raw_data.shape[1], raw_data.shape[0]*0.3125, 0],
+    ax[5].imshow(data_filtered, aspect='auto', cmap='seismic',
+                extent=[0, data_filtered.shape[1], data_filtered.shape[0]*0.3125, 0],
                 vmin=-15, vmax=15)
     ax[5].set_title('Filtered ECHO data', fontsize=fontsize_large)
     ax[5].set_ylabel('Time [ns]', fontsize=fontsize_medium)
     ax[5].tick_params(axis='both', which='major', labelsize=fontsize_small)
 
+    plt.xlim(0, raw_data.shape[1])
     fig.supxlabel('Trace number', fontsize=fontsize_medium)
 
     plt.savefig(plot_output_dir + '/' + str(sequence_id) + '_resampling_flow.png')
     plt.close()
+
+
+#* Define the plot function for CH-2A
+def plot_2A(raw_data, medf, data_filtered, sequence_id):
+    fig, ax = plt.subplots(3, 1, figsize=(12, 10), tight_layout=True, sharex=True)
+    fontsize_large = 20
+    fontsize_medium = 18
+    fontsize_small = 16
+
+    #* Plot raw data
+    ax[0].imshow(raw_data, aspect='auto', cmap='seismic',
+                extent=[0, raw_data.shape[1], raw_data.shape[0]*0.3125, 0],
+                vmin=-15, vmax=15)
+    ax[0].set_title('Raw data: ' + str(sequence_id), fontsize=fontsize_large)
+    ax[0].set_ylabel('Time [ns]', fontsize=fontsize_medium)
+    ax[0].tick_params(axis='both', which='major', labelsize=fontsize_small)
+
+    #* Plot median filtered data
+    ax[1].plot(medf)
+    ax[1].set_title('Gate with interesting data', fontsize=fontsize_large)
+    ax[1].tick_params(axis='both', which='major', labelsize=fontsize_small)
+
+    #* Plot filtered data
+    # data_filtered4plot = np.zeros((2048, data_filtered.shape[1]))
+    # data_filtered4plot[:, :data_filtered.shape[1]] = data_filtered
+    ax[2].imshow(data_filtered, aspect='auto', cmap='seismic',
+                extent=[0, data_filtered.shape[1], data_filtered.shape[0]*0.3125, 0],
+                vmin=-15, vmax=15)
+    ax[2].set_title('Filtered ECHO data', fontsize=fontsize_large)
+    ax[2].set_ylabel('Time [ns]', fontsize=fontsize_medium)
+    ax[2].tick_params(axis='both', which='major', labelsize=fontsize_small)
+
+    plt.xlim(0, raw_data.shape[1])
+    fig.supxlabel('Trace number', fontsize=fontsize_medium)
+    plt.savefig(plot_output_dir + '/' + str(sequence_id) + '_resampling_flow.png')
+    plt.close()
+
+
+
+#* Conduct resampling
+total_trace_num = 0
+resampled_trace_num = 0
+for ECHO_data in tqdm(natsorted(os.listdir(data_folder_path))):
+    #* Load ECHO data
+    if not ECHO_data.endswith('.txt'):
+        continue
+    if ECHO_data.startswith('._'):
+        continue
+
+    #* Load the data
+    ECHO_data_path = os.path.join(data_folder_path, ECHO_data)
+    raw_data = np.loadtxt(ECHO_data_path, delimiter=' ', skiprows=1) # skip the header
+    positions = raw_data[:7, :] # 7: B-scan data
+    signals = raw_data[7:, :] # 0-6: velocity and position data
+    #* Check the sequence ID
+    sequence_id = ECHO_data.split('_')[-1].split('.')[0]
+
+    print(' ')
+    print('---------------------------------')
+    print('ECHO data:', ECHO_data)
+    print('record count number:', raw_data.shape[1])
+    #* Count the number of traces before resampling
+    total_trace_num += raw_data.shape[1]
+    print('Now processing...')
+    print('   ')
+
+    #* Resampling
+    """
+    1: Not implemented yet
+    2A: Using idx list from 2B and resample according to the idx list
+    2B: Apply resampling function
+    """
+    if channel_name == '2B':
+        sobelx, med_denoised, med, medf, data_filtered, positions_filtered = resampling(signals, positions, sequence_id)
+        # data_filtered4plot = np.zeros((2048, data_filtered.shape[1]))
+        # data_filtered4plot[:, :data_filtered.shape[1]] = data_filtered
+
+        plot_2B(raw_data, sobelx, med_denoised, med, medf, data_filtered, sequence_id)
+        print(' ')
+
+    elif channel_name == '2A':
+        #* Load idx from 2B
+        idx_dir_path = '/Volumes/SSD_Kanda_SAMSUNG/LPR/LPR_2B/Resampled_Data/idx'
+        idx_file_path = os.path.join(idx_dir_path, sequence_id + '_idx.txt')
+        if not os.path.exists(idx_file_path):
+            print(f'No idx file found for sequence {sequence_id}. Skipping...')
+            continue
+        idx = np.loadtxt(idx_file_path, delimiter=' ')
+        
+        #* Make window data for plotting
+        medf = np.zeros(raw_data.shape[1])
+        medf[idx.astype(int)] = 1
+
+        #* Filter the data with the idx
+        data_filtered = signals[:, idx.astype(int)]
+        positions_filtered = positions[:, idx.astype(int)]
+        print('Raw data shape was: ', raw_data.shape)
+        print('Filtered data shape is: ', data_filtered.shape)
+        #* Save filtered data as .txt file
+        np.savetxt(txt_output_dir + '/' + str(sequence_id) + '_resampled.txt', data_filtered, delimiter=' ')
+        np.savetxt(position_output_dir + '/' + str(sequence_id) + '_resampled_position.txt', positions_filtered, delimiter=' ')
+        header_position = 'velocity, position_x, position_y, position_z, reference_point_x, reference_point_y, reference_point_z'
+        np.savetxt(position_output_dir + '/' + str(sequence_id) + '_resampled_position.txt',
+                    positions_filtered, delimiter=' ', header=header_position)
+
+        plot_2A(raw_data, medf, data_filtered, sequence_id)
+        print(' ')
+
+    elif channel_name == '1':
+        print('Channel 1 does not support resampling. Skipping...')
+        continue
+    else:
+        raise ValueError('Invalid channel name. Please enter 1, 2A, or 2B.')
+
+
+    #* Count the number of traces after resampling
+    resampled_trace_num += data_filtered.shape[1]
+    if ECHO_data == natsorted(os.listdir(data_folder_path))[-1]:
+        with open(output_dir + '/total_trace_num.txt', 'w') as f:
+            f.write('Number of total traces before resampling: ' + str(total_trace_num) + '\n')
+            f.write('Number of total traces after resampling: '+ str(resampled_trace_num))
+
+
+
